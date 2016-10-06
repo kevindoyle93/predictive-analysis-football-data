6865617465b74e8c2418e6b7cf2407a1d57c8df0
Kevin Doyle <C13547287@mydit.ie>
2016-10-06 09:49:35 +0100

    Delete previous files

--- football_data_app/data_api/data_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-	url(r'', include('football_data.urls')),
-    url(r'^admin/', admin.site.urls),
-]
--- football_data_app/data_api/football_data/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FootballDataConfig(AppConfig):
-    name = 'football_data'
