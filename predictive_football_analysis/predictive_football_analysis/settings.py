db60f9693a2d906d8e81f5a7d9e802c47aa8d94e
Kevin Doyle <C13547287@mydit.ie>
2016-10-06 10:31:57 +0100

    Add dotenv call to read settings from .env file

--- predictive_football_analysis/predictive_football_analysis/settings.py
+++ predictive_football_analysis/predictive_football_analysis/settings.py
@@ -4,7 +4,7 @@ from getenv import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
