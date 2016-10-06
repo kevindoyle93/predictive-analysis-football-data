bb2efb53b5c400141695def33aca37574a441210
Kevin Doyle <C13547287@mydit.ie>
2016-10-06 13:34:32 +0100

    Add Team and Stadium models

--- predictive_football_analysis/football_data/models.py
+++ predictive_football_analysis/football_data/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django_countries.fields import CountryField
+from geopy.distance import distance
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    country = CountryField()
+
+    def __str__(self):
+        return self.name
+
+
+class Stadium(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+    team = models.ForeignKey(Team, related_name='stadiums')
+
+    def __str__(self):
+        return '{stadium}: {team}'.format(stadium=self.name, team=self.team)
+
+    def distance_to(self, opponent_stadium):
+        location = (self.lat, self.lng)
+        opponent_location = (opponent_stadium.lat, opponent_stadium.lng)
+        return distance(location, opponent_location)
+
