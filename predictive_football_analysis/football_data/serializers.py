b11189d2d072e0aa602bcd0b01af83919511afc0
Kevin Doyle <C13547287@mydit.ie>
2016-10-10 12:28:02 +0100

    Implement hyperlinked serializers for matches

--- predictive_football_analysis/football_data/serializers.py
+++ predictive_football_analysis/football_data/serializers.py
@@ -24,5 +24,31 @@ class PlayerSerializer(serializers.ModelSerializer):
 
 
 class MatchSerializer(serializers.ModelSerializer):
+    home_team = serializers.HyperlinkedRelatedField(read_only=True, view_name='team-detail')
+    away_team = serializers.HyperlinkedRelatedField(read_only=True, view_name='team-detail')
+
+    home_player_1 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_2 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_3 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_4 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_5 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_6 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_7 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_8 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_9 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_10 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    home_player_11 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_1 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_2 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_3 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_4 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_5 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_6 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_7 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_8 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_9 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_10 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+    away_player_11 = serializers.HyperlinkedRelatedField(read_only=True, view_name='player-detail')
+
     class Meta:
         model = Match
--- predictive_football_analysis/predictive_football_analysis/urls.py
+++ predictive_football_analysis/predictive_football_analysis/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework import routers
 
-from football_data import views
-
-router = routers.DefaultRouter()
-router.register(r'league', views.LeagueViewSet)
-router.register(r'team', views.TeamViewSet)
-router.register(r'stadium', views.StadiumViewSet)
-router.register(r'player', views.PlayerViewSet)
-router.register(r'match', views.MatchViewSet)
+from football_data import urls as api_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)),
+    url(r'^api/', include(api_urls)),
 ]
