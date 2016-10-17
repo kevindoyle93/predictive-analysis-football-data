aab0fe625224eb7109a90ea95d1f3848c0424353
Kevin Doyle <C13547287@mydit.ie>
2016-10-17 10:20:57 +0100

    Format sportinglife_scraper correctly

--- predictive_football_analysis/football_data/raw_data/sportinglife_scraper.py
+++ predictive_football_analysis/football_data/raw_data/sportinglife_scraper.py
@@ -1,32 +1,32 @@
 from lxml import html
 import requests
-import json
 import re
 
-import team_names_map
+from football_data.raw_data import team_names_map
 
 BASE_URL = 'http://www.sportinglife.com'
+
 YEARS = [
-	'2011',
-	'2012',
-	'2013',
-	'2014',
-	'2015',
-	'2016',
-]
-MONTHS = [
-	'january',
-	'february',
-	'march',
-	'april',
-	'may',
-	'august',
-	'september',
-	'october',
-	'november',
-	'december',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
 ]
 
+MONTHS = [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december',
+]
 
 sport = '/football/'
 league = 'premier-league/'
@@ -38,46 +38,49 @@ root = html.fromstring(page.content)
 
 team_names = []
 for container in root.xpath('//*[@class="ixx"]'):
-	team_names.append(container[0][0].text_content())
+    team_names.append(container[0][0].text_content())
 
 match_links = []
 for match in root.xpath('//*[@class="ixxa"]'):
-	link = re.sub('commentary', 'stats', match.get('href'))
-	match_links.append(link)
+    link = re.sub('commentary', 'stats', match.get('href'))
+    match_links.append(link)
 
-match_count = 0;
+match_count = 0
 for link in match_links:
-	stats_page = html.fromstring(requests.get(BASE_URL + link).content)
-	stats_lists = stats_page.xpath('//*[@class="s-con"]')
+    stats_page = html.fromstring(requests.get(BASE_URL + link).content)
+    stats_lists = stats_page.xpath('//*[@class="s-con"]')
 
-	for stat in stats_lists[0]:
-		if isinstance(stat, html.HtmlComment):
-			continue
-		if 'Possession' in stat.text_content():
-			home_possession_str = stat.text_content()
+    home_possession_str = None
+    away_possession_str = None
 
-	for stat in stats_lists[1]:
-		if isinstance(stat, html.HtmlComment):
-			continue
-		if 'Possession' in stat.text_content():
-			away_possession_str = stat.text_content()
+    for stat in stats_lists[0]:
+        if isinstance(stat, html.HtmlComment):
+            continue
+        if 'Possession' in stat.text_content():
+            home_possession_str = stat.text_content()
 
-	home_team = team_names_map.match_team_name(
-		team_names[match_count * 2]
-	)
+    for stat in stats_lists[1]:
+        if isinstance(stat, html.HtmlComment):
+            continue
+        if 'Possession' in stat.text_content():
+            away_possession_str = stat.text_content()
 
-	away_team = team_names_map.match_team_name(
-		team_names[match_count * 2 + 1]
-	)
+    home_team = team_names_map.match_team_name(
+        team_names[match_count * 2]
+    )
 
-	home_possession = float(home_possession_str.split('%')[1][1:])
-	away_possession = float(away_possession_str.split('%')[1][1:])
-	print('{ht} {hp} - {ap} {at}'.format(
-			ht=home_team,
-			hp=home_possession, 
-			ap=away_possession,
-			at=away_team
-		)
-	)
+    away_team = team_names_map.match_team_name(
+        team_names[match_count * 2 + 1]
+    )
 
-	match_count += 1
+    home_possession = float(home_possession_str.split('%')[1][1:])
+    away_possession = float(away_possession_str.split('%')[1][1:])
+    print('{ht} {hp} - {ap} {at}'.format(
+            ht=home_team,
+            hp=home_possession,
+            ap=away_possession,
+            at=away_team,
+        )
+    )
+
+    match_count += 1
