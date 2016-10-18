ea0fafbbd5bf4640b2f41b08b450444c2dd0ddf7
Kevin Doyle <C13547287@mydit.ie>
2016-10-18 20:17:11 +0100

    Add remaining match filters

--- predictive_football_analysis/football_data/views.py
+++ predictive_football_analysis/football_data/views.py
@@ -73,8 +73,50 @@ class MatchFilter(django_filters.FilterSet):
     home_team = django_filters.CharFilter(name='home_team__name')
     away_team = django_filters.CharFilter(name='away_team__name')
 
+    min_home_full_time_goals = django_filters.NumberFilter(name='full_time_home_goals', lookup_expr='gte')
+    max_home_full_time_goals = django_filters.NumberFilter(name='full_time_home_goals', lookup_expr='lte')
+    min_away_full_time_goals = django_filters.NumberFilter(name='full_time_away_goals', lookup_expr='gte')
+    max_away_full_time_goals = django_filters.NumberFilter(name='full_time_away_goals', lookup_expr='lte')
+
+    min_home_half_time_goals = django_filters.NumberFilter(name='half_time_home_goals', lookup_expr='gte')
+    max_home_half_time_goals = django_filters.NumberFilter(name='half_time_home_goals', lookup_expr='lte')
+    min_away_half_time_goals = django_filters.NumberFilter(name='half_time_away_goals', lookup_expr='gte')
+    max_away_half_time_goals = django_filters.NumberFilter(name='half_time_away_goals', lookup_expr='lte')
+
     min_home_possession = django_filters.NumberFilter(name='home_possession', lookup_expr='gte')
     max_home_possession = django_filters.NumberFilter(name='home_possession', lookup_expr='lte')
+    min_away_possession = django_filters.NumberFilter(name='away_possession', lookup_expr='gte')
+    max_away_possession = django_filters.NumberFilter(name='away_possession', lookup_expr='lte')
+
+    min_home_shots = django_filters.NumberFilter(name='home_total_shots', lookup_expr='gte')
+    max_home_shots = django_filters.NumberFilter(name='home_total_shots', lookup_expr='lte')
+    min_away_shots = django_filters.NumberFilter(name='away_total_shots', lookup_expr='gte')
+    max_away_shots = django_filters.NumberFilter(name='away_total_shots', lookup_expr='lte')
+
+    min_home_shots_on_target = django_filters.NumberFilter(name='home_shots_on_target', lookup_expr='gte')
+    max_home_shots_on_target = django_filters.NumberFilter(name='home_shots_on_target', lookup_expr='lte')
+    min_away_shots_on_target = django_filters.NumberFilter(name='away_shots_on_target', lookup_expr='gte')
+    max_away_shots_on_target = django_filters.NumberFilter(name='away_shots_on_target', lookup_expr='lte')
+
+    min_home_corners = django_filters.NumberFilter(name='home_corners', lookup_expr='gte')
+    max_home_corners = django_filters.NumberFilter(name='home_corners', lookup_expr='lte')
+    min_away_corners = django_filters.NumberFilter(name='away_corners', lookup_expr='gte')
+    max_away_corners = django_filters.NumberFilter(name='away_corners', lookup_expr='lte')
+
+    min_home_fouls_committed = django_filters.NumberFilter(name='home_fouls_committed', lookup_expr='gte')
+    max_home_fouls_committed = django_filters.NumberFilter(name='home_fouls_committed', lookup_expr='lte')
+    min_away_fouls_committed = django_filters.NumberFilter(name='away_fouls_committed', lookup_expr='gte')
+    max_away_fouls_committed = django_filters.NumberFilter(name='away_fouls_committed', lookup_expr='lte')
+
+    min_home_yellow_cards = django_filters.NumberFilter(name='home_yellow_cards', lookup_expr='gte')
+    max_home_yellow_cards = django_filters.NumberFilter(name='home_yellow_cards', lookup_expr='lte')
+    min_away_yellow_cards = django_filters.NumberFilter(name='away_yellow_cards', lookup_expr='gte')
+    max_away_yellow_cards = django_filters.NumberFilter(name='away_yellow_cards', lookup_expr='lte')
+
+    min_home_red_cards = django_filters.NumberFilter(name='home_red_cards', lookup_expr='gte')
+    max_home_red_cards = django_filters.NumberFilter(name='home_red_cards', lookup_expr='lte')
+    min_away_red_cards = django_filters.NumberFilter(name='away_red_cards', lookup_expr='gte')
+    max_away_red_cards = django_filters.NumberFilter(name='away_red_cards', lookup_expr='lte')
 
     class Meta:
         model = Match
@@ -83,6 +125,42 @@ class MatchFilter(django_filters.FilterSet):
             'away_team',
             'full_time_result',
             'half_time_result',
+            'min_home_full_time_goals',
+            'max_home_full_time_goals',
+            'min_away_full_time_goals',
+            'min_away_full_time_goals',
+            'min_home_half_time_goals',
+            'max_home_half_time_goals',
+            'min_away_half_time_goals',
+            'min_away_half_time_goals',
+            'min_home_possession',
+            'max_home_possession',
+            'min_away_possession',
+            'max_away_possession',
+            'min_home_shots',
+            'max_home_shots',
+            'min_away_shots',
+            'max_away_shots',
+            'min_home_shots_on_target',
+            'max_home_shots_on_target',
+            'min_away_shots_on_target',
+            'max_away_shots_on_target',
+            'min_home_corners',
+            'max_home_corners',
+            'min_away_corners',
+            'max_away_corners',
+            'min_home_fouls_committed',
+            'max_home_fouls_committed',
+            'min_away_fouls_committed',
+            'max_away_fouls_committed',
+            'min_home_yellow_cards',
+            'max_home_yellow_cards',
+            'min_away_yellow_cards',
+            'max_away_yellow_cards',
+            'min_home_red_cards',
+            'max_home_red_cards',
+            'min_away_red_cards',
+            'max_away_red_cards',
         ]
 
 
