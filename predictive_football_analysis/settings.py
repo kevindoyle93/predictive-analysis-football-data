42345453feb5152b945e6d0ef129ef35113b2af4
Kevin Doyle <C13547287@mydit.ie>
2017-03-29 18:28:50 +0100

    Add gunicorn and dj-database-url packages for Heroku

--- predictive_football_analysis/settings.py
+++ predictive_football_analysis/settings.py
@@ -1,5 +1,6 @@
 import os
 
+import dj_database_url
 from getenv import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -77,6 +78,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
