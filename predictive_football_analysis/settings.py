6c7eacc459024159039270d9c2fc84dd9829df53
Kevin Doyle <C13547287@mydit.ie>
2017-04-01 15:36:03 +0100

    Setup env file to choose correct database

--- predictive_football_analysis/settings.py
+++ predictive_football_analysis/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'predictive_football_analysis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
