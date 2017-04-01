7d1c2d90aecdfe64c625778690c02cbf8def9b4b
Kevin Doyle <C13547287@mydit.ie>
2017-04-01 14:14:52 +0100

    Add STATIC_ROOT

--- predictive_football_analysis/settings.py
+++ predictive_football_analysis/settings.py
@@ -115,6 +115,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'fyp-uploaded-files/')
 MEDIA_URL = '/media/'
